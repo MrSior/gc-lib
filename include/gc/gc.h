28c8543c641d945161ef82a0006dd76bced94d9a
simon <semionchubenko@gmail.com>
2025-01-16 00:44:23 +0300

    set c++20 and wrote malloc and free gc functions

--- include/gc/gc.h
+++ include/gc/gc.h
@@ -15,26 +15,7 @@ typedef struct gc_holder {
 } gc_holder; 
 
 
-gc_holder* create_gc(pid_t tid);
-
-
-/**
- * @brief Allocates a memory block of the specified size with garbage collection support.
- * 
- * This function allocates a block of memory and registers it for management
- * by the garbage collector. The allocated memory is uninitialized.
- * 
- * @param size The size of the memory block to allocate, in bytes.
- * @return A pointer to the allocated memory block, or `nullptr` if allocation fails.
- * 
- * @note The allocated memory should not be manually freed; it will be managed
- *       by the garbage collector.
- */
-void* gc_malloc(size_t size);
-
-
-
-void gc_free(void* ptr);
+gc_holder* create_gc(pthread_t tid);
 
 
 #ifdef __cplusplus
--- src/gc.cpp
+++ src/gc.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <unordered_map>
+#include <thread>
+#include <pthread.h>
 #include "gc/gc.h"
+
+enum class ETAG : uint8_t {
+    USED,
+    NONE,
+};
 
 /**
  * @struct Allocation
@@ -14,59 +21,108 @@ struct Allocation
     void* ptr;
 
     /**
-     * @brief Pointer to a destructor function for the memory block.
-     * 
-     * The destructor is called to properly clean up the memory when it is no longer needed.
-     * 
-     * @param void* A pointer to the memory block to be destroyed.
-     */
-    void(*dtor)(void*);
-
-    /**
      * @brief Tag used during the mark phase of garbage collection or memory management.
      * 
      * This tag helps to identify the state of the memory block.
      */
-    uint8_t tag;
+    ETAG tag;
 
     /**
      * @brief Size of the allocated memory block, in bytes.
      */
     size_t size;
+
+
+    ~Allocation() {
+        free(ptr);
+    }
 };
 
-struct garbage_collector{
-    pid_t tid;
+class Garbage_collector{
+private:
+    pthread_t tid_;
+    std::unordered_map<void*, Allocation*> mem_reg_;
+    bool stop_;
+    bool quit_;
 
-    garbage_collector(pid_t tid) : tid(tid) {};
+
+    void run_mark_phase() {
+
+    }
+
+    void run_sweep_phase() {
+
+    }
+public:
+    Garbage_collector(pthread_t tid) : tid_(tid) {
+        stop_ = quit_ = true;
+    };
 
     void* malloc(size_t size) {
-        return NULL;
+        void* ptr = ::malloc(size);
+        Allocation* new_alloc = new Allocation{ptr, ETAG::NONE, size};
+        mem_reg_.insert({ptr, new_alloc});
+        return new_alloc->ptr;
     }
 
     void free(void* ptr) {
+        if (!mem_reg_.contains(ptr))
+        {
+            // TODO: throw some exception or do nothing
+        }
+        
+        delete mem_reg_[ptr];
+    }
 
+    void run() {
+        while (!quit_)
+        {
+            
+        }
+    }
+
+    ~Garbage_collector() {
+        for (const auto& elem : mem_reg_)
+        {
+            delete elem.second;
+        }
+        
     }
 };
 
 void* gc_malloc_wrapper(void* obj, size_t size) {
-    return static_cast<garbage_collector*>(obj)->malloc(size);
+    return static_cast<Garbage_collector*>(obj)->malloc(size);
 }
 
 void gc_free_wrapper(void* obj, void* ptr) {
-    static_cast<garbage_collector*>(obj)->free(ptr);
+    static_cast<Garbage_collector*>(obj)->free(ptr);
 }
 
-static std::unordered_map<pid_t, garbage_collector*> gc_register;
+static std::unordered_map<pthread_t, Garbage_collector*> gc_reg;
+static std::mutex gc_reg_mtx;
+static std::atomic<bool> need_clean;
 
-gc_holder* create_gc(pid_t tid) {
-    gc_register[tid] = new garbage_collector(tid);
+gc_holder* create_gc(pthread_t tid) {
+    if (gc_reg.contains(tid))
+    {
+        // TODO: logic for this case
+    }
+    
+    Garbage_collector* gc_ptr = new Garbage_collector(tid);
+
+    gc_reg[tid] = gc_ptr;
     gc_holder* holder =  new gc_holder();
     
     holder->malloc = &gc_malloc_wrapper;
     holder->free = &gc_free_wrapper;
-    holder->context = gc_register[tid];
+    holder->context = gc_ptr;
 
+    
+    std::thread gc_thread([](Garbage_collector* gc_ptr){
+        gc_ptr->run();
+    }, gc_ptr);
+    gc_thread.detach();
+    
     return holder;
 }
 
